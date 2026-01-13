e9a62e9dceb6d3d571ea7afc79928c993fce389b
balexey88 <balexey88@gmail.com>
2026-01-13 13:38:05 +0200

    Prepare release 0.0.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'udx/wp-stateless-wpbakery-page-builder-addon',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '56c005c8604366a4494e177fe6647eb289f7397c',
+        'reference' => '20be4b6bab5d459634a9884d57e41683b63fb398',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'udx/wp-stateless-wpbakery-page-builder-addon' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '56c005c8604366a4494e177fe6647eb289f7397c',
+            'reference' => '20be4b6bab5d459634a9884d57e41683b63fb398',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
